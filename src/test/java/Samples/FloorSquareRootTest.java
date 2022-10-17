d3a2080c68cb310cbdd0c8afe059a84a5c32cef8
yaoyao <1159776828@qq.com>
2022-10-17 21:05:13 +0800

    first change

--- src/test/java/Samples/FloorSquareRootTest.java
+++ src/test/java/Samples/FloorSquareRootTest.java
@@ -7,11 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class FloorSquareRootTest {
     @Test
     public void testA() {
-        assertEquals(3, FloorSquareRoot.floorSqrt(11));
+        assertEquals(5, FloorSquareRoot.floorSqrt(11));
     }
 
     @Test
     public void testB() {
-        assertEquals(4, FloorSquareRoot.floorSqrt(20));
+        assertEquals(6, FloorSquareRoot.floorSqrt(20));
     }
 }
